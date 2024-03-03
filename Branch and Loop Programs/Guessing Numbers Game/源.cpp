97216334642a85f71093f18cf6a857f8d388be77
zhangxy567 <zhangxy567@gmail.com>
2024-03-03 09:20:10 +0800

    1

---
+++ Branch and Loop Programs/Guessing Numbers Game/源.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
